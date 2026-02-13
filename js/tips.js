77332a0b969d8d3bd547667f9ff2a3200c933bbd
centralpathologysatna <centralpathology.satna@gmail.com>
2026-02-13 17:08:49 +0530

    Create tips.js
    added health tips section

---
+++ js/tips.js
@@ -0,0 +1,9 @@
+/* Health Tip Bar */
+.health-tip {
+    background: #eaf3ff;
+    padding: 12px;
+    text-align: center;
+    font-weight: 600;
+    color: #0b5ed7;
+    border-bottom: 1px solid #d6e6ff;
+}
