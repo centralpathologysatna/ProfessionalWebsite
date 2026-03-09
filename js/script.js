65a6a38eb71833f9fbb166c82e5f7f66e68ee271
centralpathologysatna <centralpathology.satna@gmail.com>
2026-03-09 15:58:45 +0530

    Update script.js
    aligned functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
-function submitBooking() {
-    alert("Booking submitted successfully (UI only)");
-}
 
 function toggleTests(button){
-
 const card = button.closest(".service-card");
-
 const extraTests = card.querySelectorAll(".extra-test");
-
 extraTests.forEach(test => {
-
 if(test.style.display === "list-item"){
 test.style.display = "none";
 button.innerText = "+ Show all";
 }
-
 else{
 test.style.display = "list-item";
 button.innerText = "Show less";
@@ -23,3 +15,9 @@ button.innerText = "Show less";
 });
 
 }
+
+
+function submitBooking() {
+    alert("Booking submitted successfully (UI only)");
+}
+
