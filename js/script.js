bdecc96a0cf74442ba8659af22c9a1edd5cff141
centralpathologysatna <centralpathology.satna@gmail.com>
2026-03-09 14:40:13 +0530

    Update script.js
    service card scripting added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 function submitBooking() {
     alert("Booking submitted successfully (UI only)");
 }
+
+function toggleTests(button){
+
+const card = button.closest(".service-card");
+
+const extraTests = card.querySelectorAll(".extra-test");
+
+extraTests.forEach(test => {
+
+if(test.style.display === "list-item"){
+test.style.display = "none";
+button.innerText = "+ Show all";
+}
+
+else{
+test.style.display = "list-item";
+button.innerText = "Show less";
+}
+
+});
+
+}
